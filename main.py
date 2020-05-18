0833276ba5631d92c9d9df11f8b09645443709e6
pythonclass05 <pythonclass05@yahoo.com>
2020-05-18 01:40:37 +0000

    birth year

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-#This is my First Week of python class homework
-print('''My name is Abraham
-I am in 6th grade 
-I am taking the nsf python program
-My age is 5.5 + 5.5 or''',str(5+6))
+firstname=input('Enter your firstname ')
+lastname=input('Enter your last name ')
+address=input('Enter your address ')
+zipcode=input('Enter your zipcode ')
+birthyear=int(input('Enter your birthyear '))
+age=2020-birthyear
+
+print(f'''Name: {firstname} {lastname}
+Address: {address}
+Zip: {zipcode}
+Age: {age}''')
